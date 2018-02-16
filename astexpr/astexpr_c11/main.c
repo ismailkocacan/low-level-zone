34ff4b26e817cfc7b0f9e9b463bc45afe57e9a67
ismail <ismailkocacan@gmail.com>
2018-02-16 21:33:43 +0300

    evaluate ve interpret methodları yeni yapıya göre düzenlendi.

--- astexpr/astexpr_c11/main.c
+++ astexpr/astexpr_c11/main.c
@@ -26,7 +26,7 @@ x = 1 + 2 * 3  => assignment statement
 int i = 0;
 
 typedef void* Pointer;
-
+typedef int* PInt;
 Pointer pointers[1024];
 
 enum ASTNodeType{
@@ -50,6 +50,10 @@ typedef struct ASTNode{
    struct ASTNode* right;
    enum ASTNodeType nodeType;
 } *PASTNode;
+
+typedef struct ASTOperatorNode{
+    enum ASTOperatorType value;
+} *PASTOperatorNode;
 
 typedef struct ASTVariableNode{
    char variableName[20];
@@ -75,34 +79,37 @@ Pointer createNode(enum ASTNodeType type){
     return node;
 };
 
-float evaluate(PASTExpressionNode node){
-    if (node->value != 0) return node->value;
-    PASTExpressionNode left = (PASTExpressionNode)node->astNode.left;
-    PASTExpressionNode right = (PASTExpressionNode)node->astNode.right;
-    switch (node->type) {
-        case PLUS: return evaluate(left) + evaluate(right);
-        case MINUS: return evaluate(left) - evaluate(right);
-        case DIV: return evaluate(left) / evaluate(right);
-        case MUL: return evaluate(left) * evaluate(right);
-        default:{
-            return 0.0f;
+float evaluate(PASTNode node){
+    if (!node) return 0.0f;
+    switch(node->nodeType){
+        case Operator:{
+            PASTOperatorNode operatorTypeData = (PASTOperatorNode)node->data;
+            switch (operatorTypeData->value) {
+                case Plus: return evaluate(node->left) + evaluate(node->right);
+                case Minus: return evaluate(node->left) - evaluate(node->right);
+                case Div: return evaluate(node->left) / evaluate(node->right);
+                case Mul: return evaluate(node->left) * evaluate(node->right);
+                default:{
+                    return 0.0f;
+                }
+            }
+        }
+        case Value:{
+            int value = (*(PInt)node->data);
+            if (value != 0) return value;
         }
     }
 }
 
-float interpret(PNodeX node) {
+float interpret(PASTNode node) {
     switch (node->nodeType){
         case Assignment:{
-            PASTAssignmentNode assignmentNode =  (PASTAssignmentNode)node->node;
-            PASTVariableNode variableNode = ((PASTVariableNode)assignmentNode->astNode.left);
-            PASTExpressionNode  expressionNode = ((PASTExpressionNode)assignmentNode->astNode.right);
-            // ??
-            return evaluate(expressionNode);
+            PASTNode nodeVariable = node->left;
+            PASTNode nodeOperator = node->right;
+            return evaluate(nodeOperator);
         }
-        case Expression:{
-            //struct ASTExpressionNode expressionNode = (*(PASTExpressionNode)node->node);
-            PASTExpressionNode expressionNode =  (PASTExpressionNode)node->node;
-            return evaluate(expressionNode);
+        case Operator:{
+            return evaluate(node);
         }
         default:{
             return 0.0f;
@@ -111,39 +118,56 @@ float interpret(PNodeX node) {
 }
 
 int main() {
-    PASTExpressionNode exprNodePlus = createNode(Expression);
-    exprNodePlus->type = PLUS;
-    PASTExpressionNode nodeValue_1  = createNode(Expression);
-    nodeValue_1->value = 1;
+    PASTNode nodeAssignment,
+            nodeVariable,
+            nodeOperatorPlus,
+            nodeOperatorMul,
+            nodeValue_1,
+            nodeValue_2,
+            nodeValue_3;
 
-    PASTExpressionNode nodeMul = createNode(Expression);
-    nodeMul->type = MUL;
-    PASTExpressionNode nodeValue_2  = createNode(Expression);
-    nodeValue_2->value = 2;
-    PASTExpressionNode nodeValue_3  = createNode(Expression);
-    nodeValue_3->value = 3;
+    nodeAssignment = createNode(Assignment);
 
-    exprNodePlus->astNode.left = nodeValue_1;
-    exprNodePlus->astNode.right = nodeMul;
-    nodeMul->astNode.left = nodeValue_2;
-    nodeMul->astNode.right = nodeValue_3;
+    nodeVariable = createNode(Variable);
+    PASTVariableNode varNode = new(sizeof(struct ASTVariableNode));
+    strcpy(varNode->variableName,"x");
+    nodeVariable->data = varNode;
 
-    PASTAssignmentNode assignmentNode = createNode(Assignment);
-    PASTVariableNode variableNode = createNode(Variable);
-    strcpy(variableNode->variableName,"x");
-    assignmentNode->astNode.left = variableNode;
-    assignmentNode->astNode.right =  exprNodePlus;
+    nodeOperatorPlus = createNode(Operator);
+    PASTOperatorNode opDataPlus = new(sizeof(struct ASTOperatorNode));
+    opDataPlus->value = Plus;
+    nodeOperatorPlus->data = opDataPlus;
 
-    float result = evaluate(exprNodePlus);
+    nodeOperatorMul = createNode(Operator);
+    PASTOperatorNode opDataMul = new(sizeof(struct ASTOperatorNode));
+    opDataMul->value = Mul;
+    nodeOperatorMul->data = opDataMul;
+
+    nodeValue_1 = createNode(Value);
+    nodeValue_1->data = new(sizeof(int));
+    (*(PInt)nodeValue_1->data) = 1;
+
+    nodeValue_2 = createNode(Value);
+    nodeValue_2->data = new(sizeof(int));
+    (*(PInt)nodeValue_2->data) = 2;
+
+    nodeValue_3 = createNode(Value);
+    nodeValue_3->data = new(sizeof(int));
+    (*(PInt)nodeValue_3->data) = 3;
+
+    nodeOperatorPlus->left  = nodeValue_1;
+    nodeOperatorPlus->right = nodeOperatorMul;
+    nodeOperatorMul->left = nodeValue_2;
+    nodeOperatorMul->right = nodeValue_3;
+
+    nodeAssignment->left = nodeVariable;
+    nodeAssignment->right = nodeOperatorPlus;
+
+    float result = evaluate(nodeOperatorPlus);
     printf("result : %f",result);
 
-    struct NodeX node1 = { exprNodePlus, Expression };
-    float result1 = interpret(&node1);
+    float result1 = interpret(nodeAssignment);
     printf("result1 : %f",result1);
-
-    struct NodeX node2 = { assignmentNode, Assignment };
-    float result2 = interpret(&node2);
-    printf("result2 : %f",result2);
 
     int count = sizeof(pointers) / sizeof(Pointer);
     for (int j = 0; j < count; j++) {

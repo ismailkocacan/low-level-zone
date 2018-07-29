149a0955951e7366bd498b82db165a44cff0a3fd
ismail <ismailkocacan@gmail.com>
2018-07-29 17:00:51 +0300

    first attempt

--- dynamicArrayImp/Cpp/GenericArrayImp/GenericArrayImp/main.cpp
+++ dynamicArrayImp/Cpp/GenericArrayImp/GenericArrayImp/main.cpp
@@ -1,6 +1,32 @@
 
+#include <iostream>
+
+template <class Type>
+class DynamicArray
+{
+private:
+	int fLength;
+	Type* fMemBlock;
+public:
+	DynamicArray(size_t length) :
+		fLength(length) {
+		fMemBlock = (Type*)malloc(sizeof(Type) * fLength);
+	}
+	~DynamicArray() {
+		free(fMemBlock);
+	}
+public:
+	Type GetElement(int index) {
+
+	}
+
+	Type SetElement(int index) {
+
+	}
+};
+
 int main() {
-
-
+	DynamicArray<int> myArray(2);
+	std::cout << "hede hedöö" << std::endl;
 	return 0;
 }

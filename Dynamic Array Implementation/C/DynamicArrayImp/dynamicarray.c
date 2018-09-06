13f92837a5b17030e30c41ced225d369d38e8975
ismail <ismailkocacan@gmail.com>
2018-09-07 01:10:53 +0300

    don't repeat your self

--- Dynamic Array Implementation/C/DynamicArrayImp/dynamicarray.c
+++ Dynamic Array Implementation/C/DynamicArrayImp/dynamicarray.c
@@ -5,15 +5,19 @@ PArray NewArray(int count) {
 	return malloc(size);
 }
 
+int Offset(int index) {
+	return sizeof(int) * index;
+}
+
 void SetArrayElement(PArray anArray, int index, int value) {
 	// check out of range
-	int* p = (anArray + (sizeof(int) * index));
+	int* p = (anArray + Offset(index));
 	*p = value;
 }
 
 int GetArrayElement(PArray anArray, int index) {
 	// check out of range
-	int* p = (anArray + (sizeof(int) * index));
+	int* p = (anArray + Offset(index));
 	return *p;
 }
 

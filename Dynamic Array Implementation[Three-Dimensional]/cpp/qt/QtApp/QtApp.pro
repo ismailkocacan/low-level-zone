60b20f480123270825b181be206218d75045206c
ismailkocacan <ismailkocacan@gmail.com>
2019-11-28 23:53:52 +0300

    QT MinGW compile test

--- Dynamic Array Implementation[Three-Dimensional]/cpp/qt/QtApp/QtApp.pro
+++ Dynamic Array Implementation[Three-Dimensional]/cpp/qt/QtApp/QtApp.pro
@@ -21,3 +21,7 @@ SOURCES += \
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+HEADERS +=
+
+INCLUDEPATH += "../../mingw"
--- Dynamic Array Implementation[Three-Dimensional]/cpp/qt/QtApp/main.cpp
+++ Dynamic Array Implementation[Three-Dimensional]/cpp/qt/QtApp/main.cpp
@@ -1,8 +1,26 @@
 #include <QCoreApplication>
+#include <threedimensionalarray.hpp>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    ThreeDimensionalArray<int32_t> anArray(2,2,2); // 2 adet, 2 sütun, 2 satırlık tablo
+    // access elements ( set)
+    for (size_t depthIndex = 0; depthIndex < anArray.GetDepthSize(); depthIndex++)
+         for (size_t colIndex = 0; colIndex < anArray.GetColCount(); colIndex++)
+             for (size_t rowIndex = 0; rowIndex < anArray.GetRowCount(); rowIndex++)
+                     anArray.SetElement(depthIndex,colIndex,rowIndex, depthIndex + colIndex + rowIndex);
 
+    // access elements ( get)
+    for (size_t depthIndex = 0; depthIndex < anArray.GetDepthSize(); depthIndex++)
+         for (size_t colIndex = 0; colIndex < anArray.GetColCount(); colIndex++)
+             for (size_t rowIndex = 0; rowIndex < anArray.GetRowCount(); rowIndex++)
+                   std::cout <<  anArray.GetElement(depthIndex,colIndex,rowIndex) << std::endl;
+
+    // access elements via indexer
+    Index index = { 0, 0, 0 };
+    int32_t val = anArray[&index];
+    anArray[&index] = 31;
+
+    QCoreApplication a(argc, argv);
     return a.exec();
 }

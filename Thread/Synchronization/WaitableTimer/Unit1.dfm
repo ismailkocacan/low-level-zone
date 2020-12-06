e3c2f912339a69f15c7b04b40ab21077d96ecfee
ismailkocacan <ismailkocacan@gmail.com>
2020-12-06 03:24:47 +0300

    WaitableTimers

---
+++ Thread/Synchronization/WaitableTimer/Unit1.dfm
@@ -0,0 +1,33 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 467
+  ClientWidth = 710
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 120
+    Top = 48
+    Width = 449
+    Height = 137
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Memo1: TMemo
+    Left = 104
+    Top = 208
+    Width = 529
+    Height = 233
+    ScrollBars = ssVertical
+    TabOrder = 1
+  end
+end
---
+++ Thread/Synchronization/WaitableTimer/Unit1.pas
@@ -0,0 +1,87 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  ASystemTime : SYSTEMTIME;
+  ALocalTime : FILETIME;
+  AUtcTime : FILETIME;
+
+  lpDueTime: Int64;
+  WaitResult : Cardinal;
+  TimerHandle: THandle;
+begin
+
+  TimerHandle := CreateWaitableTimer(nil, true, nil);
+  if TimerHandle <> 0 then
+  begin
+      ASystemTime.wYear := 2020;
+      ASystemTime.wMonth := 12;
+      ASystemTime.wDay := 6;
+      ASystemTime.wHour := 3;
+      ASystemTime.wMinute := 23;
+      ASystemTime.wSecond := 0;
+      ASystemTime.wMilliseconds := 0;
+
+      SystemTimeToFileTime(ASystemTime, ALocalTime);
+      LocalFileTimeToFileTime(ALocalTime,AUtcTime);
+
+      LARGE_INTEGER(lpDueTime).LowPart := AUtcTime.dwLowDateTime;
+      LARGE_INTEGER(lpDueTime).HighPart := AUtcTime.dwHighDateTime;
+
+      SetWaitableTimer(TimerHandle, lpDueTime, 0, nil, nil, false);
+
+      TThread.CreateAnonymousThread(
+       procedure
+       begin
+
+          TThread.Queue(nil,
+           procedure
+           begin
+             Memo1.Lines.Add('>>>>>>>>> waiting');
+           end
+          );
+
+          WaitResult := WaitForSingleObject(TimerHandle, INFINITE);
+
+          TThread.Queue(nil,
+           procedure
+           begin
+             Memo1.Lines.Add('>>>>>>>>> worked at '+ FormatDateTime('dd.MM.yyyy hh:mm:ss',Now()));
+           end
+          );
+
+          CloseHandle(WaitResult);
+
+       end
+      ).Start;
+  end;
+
+
+end;
+
+end.

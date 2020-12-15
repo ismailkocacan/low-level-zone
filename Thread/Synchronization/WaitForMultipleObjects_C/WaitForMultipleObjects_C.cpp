271fa3bd047aa80fb1e6c1871c410fc4621842d0
ismailkocacan <ismailkocacan@gmail.com>
2020-12-15 22:33:10 +0300

    devam edilecek

--- Thread/Synchronization/WaitForMultipleObjects_C/WaitForMultipleObjects_C.cpp
+++ Thread/Synchronization/WaitForMultipleObjects_C/WaitForMultipleObjects_C.cpp
@@ -1,6 +1,51 @@
-#include <iostream>
+﻿#include <iostream>
+#include <Windows.h>
+
+#define EVENT_NAME L"KFLAG"
+
+const int cThreadCount = 100;
+HANDLE ThreadHandles[cThreadCount - 1];
+HANDLE WaitObjects[cThreadCount - 1];
+
+VOID NTAPI WaitOrTimerCallback(PVOID lpParameter, BOOLEAN TimerOrWaitFired) {
+
+}
+
+
+DWORD WINAPI ThreadStartFunction(LPVOID lpThreadParameter) {
+    return 0;
+}
+
+
 
 int main()
 {
+    HANDLE hEventObject = CreateEvent(NULL, true, false, EVENT_NAME);
+    if (hEventObject == 0)
+        return 0;
+    ResetEvent(hEventObject);
+
+    int length = sizeof(ThreadHandles) / sizeof(ThreadHandles[0]);
+    for (size_t i = 0; i < length; i++)
+    {
+        DWORD lpThreadId = 0;
+        HANDLE threadHandle = CreateThread(NULL,
+                                            0,
+                                            &ThreadStartFunction,
+                                            hEventObject,
+                                            0,
+                                            &lpThreadId);
+        ThreadHandles[i] = threadHandle;
+
+        HANDLE hWaitObject = 0;
+        if (RegisterWaitForSingleObject(&hWaitObject,
+                                        threadHandle,
+                                        &WaitOrTimerCallback,
+                                        &threadHandle,
+                                        INFINITE,
+                                        WT_EXECUTEONLYONCE))
+            WaitObjects[i] = hWaitObject;
+    }
+
     std::cout << "Hello World!\n";
 }
